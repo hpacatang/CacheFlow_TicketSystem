568c4308d6968aa3f112ba5ce3d3e966f96ff96b
PatDep-0 <pawpawmakitsu@gmail.com>
2025-04-26 10:07:37 +0800

    minor inputs
    placed some code for the routing, aka forgor to add them before :)

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,6 +9,8 @@ export const AppRoutes = () => {
         <Route path={PATHS.MAIN.path} element={<Views.Main />}>
           {/* ADD PPRIVATE ROUTES HERE (Routes that can only access after login like Dashboard, Account Setting, etc.) */}
           <Route path={PATHS.DASHBOARD.path} element={<Views.Dashboard />} />
+          <Route path={PATHS.DASHBOARD.path} element={<Views.TicketDash />} />
+          <Route path={PATHS.DASHBOARD.path} element={<Views.KnowBase />} />
         </Route>
         {/* ADD PUBLIC ROUTES HERE (e.g., Login, Sign Up, Forgot Pass, etc. ) */}
         <Route path={PATHS.LOGIN.path} element={<Views.Login />} />
---
+++ src/views/containers/KnowledgeBase/KnowBase.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const KnowBase = () => {
+  return (
+    <div>KnowBase</div>
+  )
+}
